a1f81cc1cd7926c02c80826a1b34808727b61566
Everlyne akinyi <evahaudi2354@gmail.com>
2023-11-24 08:40:09 +0000

    adeed

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,5 @@
-# Installs puppet-lint, version 2.5.0
-
-package { 'puppet-lint':
-  ensure   => '2.5.0',
-  provider => 'gem',
-}
+# install flask from pip3
+package { 'flask' :
+    ensure   => '2.1.0',
+    provider => 'pip3',
+    }
