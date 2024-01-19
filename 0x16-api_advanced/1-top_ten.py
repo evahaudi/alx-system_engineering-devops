6483b9e12e0e0e52b05989176940ad302ef08d53
everlyne akinyi <evahaudi2354@gmail.com>
2024-01-19 03:26:45 -0800

    updated

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,39 +1,21 @@
 #!/usr/bin/python3
-"""
-A Function that queries the Reddit API and prints
-the top ten hot posts of a subreddit
-"""
+"""Function to print hot posts on a given Reddit subreddit."""
 import requests
 
 
 def top_ten(subreddit):
-    """Queries to Reddit API"""
-    # Updated User-Agent, client ID, and client Secret
-    u_agent = 'evah/1.0 (by /u/Rude-Entrepreneur52)'
-    client_id = 'vkP5KBd-vWbZS_9JvQkzRw'
-    client_secret = 'BmDfHc4nRAHa0GiG9B2M3ZwQfI4HMA'
-
+    """Print the titles of the 10 hottest posts on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
     headers = {
-        'User-Agent': u_agent,
-        'Authorization': f'Basic {client_id}:{client_secret}'
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     }
-
     params = {
-        'limit': 10
+        "limit": 10
     }
-
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
-    res = requests.get(url,
-                       headers=headers,
-                       params=params,
-                       allow_redirects=False)
-    if res.status_code != 200:
-        print(None)
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
+    if response.status_code == 404:
+        print("None")
         return
-    dic = res.json()
-    hot_posts = dic['data']['children']
-    if len(hot_posts) == 0:
-        print(None)
-    else:
-        for post in hot_posts:
-            print(post['data']['title'])
+    results = response.json().get("data")
+    [print(c.get("data").get("title")) for c in results.get("children")]
